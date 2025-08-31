1f2705a5946e2dde44c95701c5dc0a80267e5b17
lisette-espin <leespinn@gmail.com>
2025-08-31 13:28:12 +0200

    enhancing rep. bias plots adding GT bars

--- Auditor/code/libs/factuality/factcheck.py
+++ Auditor/code/libs/factuality/factcheck.py
@@ -116,31 +116,38 @@ class FactualityCheck(object):
     def get_minority_baselines(attribute, df_gt_stats, df_all_authors_demographics, df_all_authors_stats):
         from libs.factuality import author
 
-        minority = constants.GENDER_FEMALE if attribute == 'gender' else constants.ETHNICITY_BLACK
+        # minority = constants.GENDER_FEMALE if attribute == 'gender' else constants.ETHNICITY_BLACK
+        groups = constants.GENDER_LIST if attribute == 'gender' else constants.ETHNICITY_LIST
 
         # Field
-        baseline_field = df_gt_stats.query("label in ['Condensed Matter & Materials Physics', 'Physics Education Research']")[f'{attribute}_fractions'][minority]
+        baseline_field = df_gt_stats.query("label in ['Condensed Matter & Materials Physics', 'Physics Education Research']")[f'{attribute}_fractions'][groups]
         baseline_field.rename(index={'Condensed Matter & Materials Physics':'CMMP', 'Physics Education Research':'PER'}, inplace=True)
 
         # Epoch
         df_periods = df_all_authors_stats.set_index('id_author_oa').join(df_all_authors_demographics[['id_author','gender', 'ethnicity']].set_index('id_author'))
         df_periods = df_periods[['min_year', 'max_year', 'career_age', 'gender', 'ethnicity']]
         decades = [1950, 2000]
-        fvalues = []
+
+        baseline_epoch = author.pd.DataFrame()
         for decade in decades:
             k = f"s{decade}s"
             df_periods.loc[:,k] = df_periods.apply(lambda row: not ((row.max_year < decade) or (row.min_year > decade + 10)) , axis=1)
             tmp = df_periods.query(f"{k} == True")
-            f = tmp.query(f"{attribute}=='{minority}'").shape[0] / tmp.shape[0]
-            fvalues.append(f)
-        baseline_epoch = io.pd.Series(fvalues, index=decades)
+            normalized_counts = author.pd.DataFrame(
+                tmp.groupby(attribute)
+                .size()
+                .div(len(tmp))  # divide by total rows
+            ).T
+            normalized_counts.loc[:, 'decade'] = decade
+            baseline_epoch = author.pd.concat([baseline_epoch, normalized_counts])
+        baseline_epoch.set_index('decade', inplace=True)
 
         # Seniority
         df_periods.loc[:, 'seniority'] = df_periods.career_age.apply(lambda v: author.get_seniority(v))
         counts = df_periods.groupby(['seniority', attribute]).size().reset_index(name='count')
         counts['fraction'] = counts['count'] / counts.groupby('seniority')['count'].transform('sum')
         result = counts.pivot(index='seniority', columns=attribute, values='fraction')
-        baseline_seniority = result.loc[constants.TASK_SENIORITY_PARAMS, minority]
+        baseline_seniority = result.loc[constants.TASK_SENIORITY_PARAMS, groups]
 
         return {constants.EXPERIMENT_TASK_FIELD: baseline_field, 
                 constants.EXPERIMENT_TASK_EPOCH: baseline_epoch,
