de386c0dd3074c9ee0f2502ff528c547e1ef2327
Lisette Espin <leespinn@gmail.com>
2025-10-21 19:52:35 +0200

    loading new results from openrouter and new models
    adapting to new types of errors, specially when temperature > 1

--- Auditor/code/preprocessing/batch_valid_answers.py
+++ Auditor/code/preprocessing/batch_valid_answers.py
@@ -15,14 +15,14 @@ from libs import text
 def run(experiments_dir: str, model: str, max_workers: int, output_dir: str):
    # process experiments
    results = experiments.read_experiments(experiments_dir, model, max_workers)
-   df = pd.DataFrame(results).drop(columns=['result_answer']).sort_values(by=['date', 'time', 'task_name', 'task_param'])
+   df = pd.DataFrame(results).drop(columns=['result_answer']).sort_values(by=['temperature', 'date', 'time', 'task_name', 'task_param'])
 
    # process each reponse (json)
-   df_valid_responses = parallel_processing(results, max_workers).sort_values(by=['date', 'time', 'task_name', 'task_param'])
+   df_valid_responses = parallel_processing(results, max_workers).sort_values(by=['temperature', 'date', 'time', 'task_name', 'task_param'])
    df_valid_responses.loc[:,'clean_name'] = df_valid_responses.loc[:,'name'].apply(lambda x: text.clean_name(text.replace(x, constants.EXPERIMENT_AUDIT_FACTUALITY_AUTHOR_NAME_REPLACEMENTS)))
 
    # Update responses with valid_attempt that end up invalid
-   col_ids = ['llm_model','date','time','task_name','task_param','task_attempt']
+   col_ids = ['model', 'temperature', 'llm_provider', 'llm_model','date','time','task_name','task_param','task_attempt']
    df = df.set_index(col_ids)
    tmp = df.join(df_valid_responses[col_ids + ['name']].set_index(col_ids), how='left')
    ids = tmp.query("@io.pd.isnull(name) and result_valid_flag in @constants.EXPERIMENT_OUTPUT_VALID_FLAGS").index
@@ -38,7 +38,7 @@ def run(experiments_dir: str, model: str, max_workers: int, output_dir: str):
    io.save_csv(df, fn, index=False)
 
    # Add metadata to valid responses 
-   df_valid_responses = df_valid_responses.merge(df[['llm_model','date','time','task_name','task_param','task_attempt','model','valid_attempt']], on=['llm_model','date','time','task_name','task_param','task_attempt'], how='left')
+   df_valid_responses = df_valid_responses.merge(df[['model','temperature','llm_model','date','time','task_name','task_param','task_attempt','valid_attempt']], on=['model', 'temperature', 'llm_model', 'date','time','task_name','task_param','task_attempt'], how='left')
    df_valid_responses = df_valid_responses.query("valid_attempt == True")
    fn = io.path_join(output_dir, 'valid_responses', f"{model}.csv")
    io.validate_path(fn)
@@ -57,6 +57,7 @@ def process_results(results):
     rows = []
 
     for obj in results:
+
         result_valid_flag = obj['result_valid_flag']
         result_answer = obj['result_answer']
 
@@ -65,13 +66,16 @@ def process_results(results):
                 name = answer.get("Name", None)
                 
                 if name is None or pd.isnull(name) or len(name) >= 100 or name.lower() in constants.EXPERIMENTS_BAD_CONTENT_TEXTS or name == 'None':
-                    io.printf(f"skipping: {obj['date']} | {obj['time']} | {obj['llm_model']} | {obj['task_name']} | {obj['task_param']} | {obj['task_attempt']}")
+                    io.printf(f"skipping: {obj['date']} | {obj['time']} | {obj['llm_model']} | {obj['temperature']} | {obj['task_name']} | {obj['task_param']} | {obj['task_attempt']}")
                     print(name)
                     continue
 
                 rows.append({
                     "date": obj['date'],
                     "time": obj['time'],
+                    "model": obj['model'],
+                    "temperature": obj['temperature'],
+                    "llm_provider": obj['llm_provider'],
                     "llm_model": obj['llm_model'],
                     "task_name": obj['task_name'],
                     "task_param": obj['task_param'],
@@ -91,8 +95,8 @@ def process_results(results):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiments_dir", required=True, type=str, help="Directory where the experiment data is stored")
-    parser.add_argument("--model", required=True, type=str, choices=constants.LLMS, help="Model to analyse (eg., gemma2-9b llama-3.1-8b llama-3.1-70b llama3-8b llama3-70b mixtral-8x7b)")
+    parser.add_argument("--experiments_dir", required=True, type=str, help="Directory where the experiment data is stored (root of temperature_<value>/)")
+    parser.add_argument("--model", required=True, type=str, choices=constants.LLMS, help=f"Model to analyse (ie., {', '.join(constants.LLMS)})")
     parser.add_argument("--max_workers", type=int, default=1, help="How many jobs to run in parallel maximum")
     parser.add_argument("--output_dir", required=True, type=str, help="Directory where the output files will be saved")
     args = parser.parse_args()
