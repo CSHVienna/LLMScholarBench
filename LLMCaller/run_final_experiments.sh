38e5fb1424415a9e46e9a39abdbe793279f2e471
DanieleBarolo <barolo@csh.ac.at>
2025-12-17 11:34:59 +0100

    update conda path in run_final_experiments.sh

--- LLMCaller/run_final_experiments.sh
+++ LLMCaller/run_final_experiments.sh
@@ -4,9 +4,9 @@
 set -e
 cd /code/barolo/LLMScholar-Audits/LLMCaller
 
-# Activate conda
-eval "$(conda shell.bash hook)"
-conda activate py312-llmcaller
+# Activate conda using full path (needed for cron environment)
+eval "$(/home/netin/anaconda3/bin/conda shell.bash hook)"
+/home/netin/anaconda3/bin/conda activate py312-llmcaller
 
 # Run experiments (paths read from config/paths.json)
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting experiments..."
