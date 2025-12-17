736e21569de7e1495edad59af63be480e4e82f7d
DanieleBarolo <barolo@csh.ac.at>
2025-12-17 11:45:55 +0100

    add directly sources the conda.sh for the conda env activation in cron

--- LLMCaller/run_final_experiments.sh
+++ LLMCaller/run_final_experiments.sh
@@ -5,8 +5,8 @@ set -e
 cd /code/barolo/LLMScholar-Audits/LLMCaller
 
 # Activate conda using full path (needed for cron environment)
-eval "$(/home/netin/anaconda3/bin/conda shell.bash hook)"
-/home/netin/anaconda3/bin/conda activate py312-llmcaller
+source /home/netin/anaconda3/etc/profile.d/conda.sh
+conda activate py312-llmcaller
 
 # Run experiments (paths read from config/paths.json)
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting experiments..."
